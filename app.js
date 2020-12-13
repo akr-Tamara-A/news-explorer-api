580757a91df30344a64cd5943b7e70d2742b17f5
Tamara A <akira-49-q@yandex.ru>
2020-12-13 15:52:45 +0200

    feat: добавить express-rate-limit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
@@ -11,12 +12,18 @@ const errorsHendler = require('./middlewares/errorsHendler');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/newsdb' } = process.env;
 
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(rateLimiter);
 
 /** подключаемся к серверу mongo */
 mongoose.connect(MONGO_URL, {
